157fe814816aa46c5fe18aaa0748e992e339bee2
Shota Kuroki <kurokis@users.noreply.github.com>
2017-07-27 10:43:24 +0900

    added autogen plot

--- log_viewer.py
+++ log_viewer.py
@@ -9,44 +9,12 @@ if __name__ == '__main__':
 
     # parameters
     log_filename = sys.argv[1]
-    log_format_filename = 'tools/log_format.json'
+    log_format_filename = "./tools/log_format.json"
+    settings_filename = "./tools/settings.json"
 
     # load log file
-    print("Loading \""+log_filename+"\" using log format defined in \""+log_format_filename+"\"")
+    print("Loading \""+log_filename)
     dfs = load_log_file(log_filename, log_format_filename)
     print("File load successful")
 
-    print(dfs)
-
-    # plot flightctrl data
-    df_flightctrl = None
-    try:
-        df_flightctrl = dfs[[df.index.name for df in dfs].index('flightctrl')]
-    except ValueError:
-        print("\'flightctrl\' not in log file")
-    if df_flightctrl is not None:
-        fig = plt.figure()
-        plot_flightctrl(fig, df_flightctrl, log_format_filename)
-
-    # plot vision data
-    df_vision = None
-    try:
-        df_vision = dfs[[df.index.name for df in dfs].index('vision')]
-    except ValueError:
-        print("\'vision\' not in log file")
-    if df_vision is not None:
-        fig2 = plt.figure()
-        plot_vision(fig2, df_vision, log_format_filename)
-
-    # plot debug data
-    df_debug = None
-    try:
-        df_debug = dfs[[df.index.name for df in dfs].index('debug')]
-    except ValueError:
-        print("\'debug\' not in log file")
-    if df_debug is not None:
-        fig3 = plt.figure()
-        plot_debug(fig3, df_debug, log_format_filename)
-
-    # show plot
-    plt.show()
+    plot_dataframes(dfs, log_format_filename, settings_filename)
--- tools/tools.py
+++ tools/tools.py
@@ -1,13 +1,8 @@
 import json
 import csv
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-### BEGIN PARAMETERS ###
-my_xlim = (838,842)
-use_my_xlim = False
-### END PARAMETERS ###
 
 def load_log_file(log_filename, log_format_filename):
     log_format = json.load(open(log_format_filename))
@@ -54,243 +49,69 @@ def load_log_file(log_filename, log_format_filename):
             pass
     return dfs
 
-def plot_flightctrl(fig, df, log_format_filename):
-    # get y-axis labels
+def plot_dataframes(dfs, log_format_filename, settings_filename):
+    # dfs: list of pandas dataframes
+    settings = json.load(open(settings_filename))
+    logtypes = list(settings["log_viewer"]["plot"].keys())
+
+    # logtype is inserted in logtypes_present if
+    # 1. plot is enabled in settings, and
+    # 2. data for this logtype is present in the given dfs
+    logtypes_present = [logtype for logtype in logtypes if (settings["log_viewer"]["plot"][logtype]==1 and (logtype in [df.index.name for df in dfs]))]
+
+    print("Plotting:", logtypes_present)
+    figs = [plt.figure() for i in range(len(logtypes_present))]
+    for i,logtype in enumerate(logtypes_present):
+        df = dfs[[df.index.name for df in dfs].index(logtype)]
+        autogen_plot(figs[i], df, log_format_filename, settings_filename)
+
+    plt.show()
+
+def autogen_plot(fig, df, log_format_filename, settings_filename):
+    # find corrensponding log id
+    logtype = df.index.name
+    id = get_corresponding_id(logtype, log_format_filename)
+
+    # extract x-axis data
     log_format = json.load(open(log_format_filename))
-    for id in log_format["id"].keys():
-        if log_format["id"][id]["logtype"] == 'flightctrl':
-            labels = log_format["id"][id]["labels"]
-            units = log_format["id"][id]["units"]
-            ylabels = {}
-            for label,unit in zip(labels,units):
-                ylabels[label] = unit
-            break
+    axes = log_format["id"][id]["axes"]
+    idx_xaxis = axes.index(0)
+    xaxis_label = log_format["id"][id]["labels"][idx_xaxis]
+    xunit = log_format["id"][id]["units"][idx_xaxis]
+    x_axis_data = np.array(df[xaxis_label])
+    if xunit == "microseconds":
+        x_axis_data /= 1000000.0
+        xunit = "Time (s)"
+    else:
+        xunit = xaxis_label
 
-    t = np.array(df["timestamp"])/1000000.
-    nmr = np.array(df["nav mode request"])
-    fcs = np.array(df["flightctrl state"])
-    ax = np.array(df["accelerometer x"])
-    ay = np.array(df["accelerometer y"])
-    az = np.array(df["accelerometer z"])
-    wx = np.array(df["gyro x"])
-    wy = np.array(df["gyro y"])
-    wz = np.array(df["gyro z"])
-    q0 = np.array(df["quaternion 0"])
-    qx = np.array(df["quaternion x"])
-    qy = np.array(df["quaternion y"])
-    qz = np.array(df["quaternion z"])
-    pa = np.array(df["pressure altitude"])
+    # plot
+    labels = log_format["id"][id]["labels"]
+    units = log_format["id"][id]["units"]
+    axes = log_format["id"][id]["axes"]
+    axes_layout = log_format["id"][id]["axes_layout"]
+    for i in range(1,max(axes)+1):
+        ax = fig.add_subplot(axes_layout[0], axes_layout[1], i)
 
-    # Nav mode request
-    ax1 = fig.add_subplot(231)
-    ax1.plot(t,nmr)
-    ax1.grid()
-    ax1.set_xlabel("time (s)")
-    ax1.set_title("Nav Mode Request")
+        # find label for y-axis
+        for ax_num, label, unit in zip(axes, labels, units):
+            if ax_num == i: # plot in this ax
+                y_axis_data = np.array(df[label])
+                ax.plot(x_axis_data, y_axis_data)
+                ax.grid(True)
+                ax.set_xlabel(xunit)
+                ax.set_ylabel(unit)
+                ax.set_title(label)
 
-    # Flightctrl request
-    ax2 = fig.add_subplot(232)
-    ax2.plot(t,fcs)
-    ax2.grid()
-    ax2.set_xlabel("time (s)")
-    ax2.set_title("FlightCtrl State")
+        # Change xlim
+        settings = json.load(open(settings_filename))
+        use_xlim = int(settings["log_viewer"]["use_xlim"])
+        xlim = settings["log_viewer"]["xlim"]
+        if use_xlim:
+            ax.set_xlim(xlim)
 
-    # Accelerometer
-    ax3 = fig.add_subplot(233)
-    ax3.plot(t,ax)
-    ax3.plot(t,ay)
-    ax3.plot(t,az)
-    ax3.grid()
-    ax3.set_xlabel("time (s)")
-    ax3.set_ylabel(ylabels["accelerometer x"])
-    ax3.set_title("Accelerometer")
-
-    # Gyro
-    ax4 = fig.add_subplot(234)
-    ax4.plot(t,wx)
-    ax4.plot(t,wy)
-    ax4.plot(t,wz)
-    ax4.grid()
-    ax4.set_xlabel("time (s)")
-    ax4.set_ylabel(ylabels["gyro x"])
-    ax4.set_title("Gyro")
-
-    # Quaternion
-    ax5 = fig.add_subplot(235)
-    ax5.plot(t,q0)
-    ax5.plot(t,qx)
-    ax5.plot(t,qy)
-    ax5.plot(t,qz)
-    ax5.grid()
-    ax5.set_xlabel("time (s)")
-    ax5.set_title("Quaternion")
-
-    # Pressure altitude
-    ax6 = fig.add_subplot(236)
-    ax6.plot(t,pa)
-    ax6.grid()
-    ax6.set_xlabel("time (s)")
-    ax6.set_ylabel(ylabels["pressure altitude"])
-    ax6.set_title("Pressure Altitude")
-
-    # Change xlim
-    if use_my_xlim:
-        ax1.set_xlim(my_xlim)
-        ax2.set_xlim(my_xlim)
-        ax3.set_xlim(my_xlim)
-        ax4.set_xlim(my_xlim)
-        ax5.set_xlim(my_xlim)
-        ax6.set_xlim(my_xlim)
-
-def plot_vision(fig, df, log_format_filename):
-    # get y-axis labels
+def get_corresponding_id(logtype, log_format_filename):
     log_format = json.load(open(log_format_filename))
-    for id in log_format["id"].keys():
-        if log_format["id"][id]["logtype"] == 'vision':
-            labels = log_format["id"][id]["labels"]
-            units = log_format["id"][id]["units"]
-            ylabels = {}
-            for label,unit in zip(labels,units):
-                ylabels[label] = unit
-            break
-
-    t = np.array(df["timestamp"])/1000000.
-    rx = np.array(df["position x"])
-    ry = np.array(df["position y"])
-    rz = np.array(df["position z"])
-    qx = np.array(df["quaternion x"])
-    qy = np.array(df["quaternion y"])
-    qz = np.array(df["quaternion z"])
-    rvarx = np.array(df["r var x"])
-    rvary = np.array(df["r var y"])
-    rvarz = np.array(df["r var z"])
-    st = np.array(df["status"])
-
-    # Position
-    ax1 = fig.add_subplot(221)
-    ax1.plot(t,rx)
-    ax1.plot(t,ry)
-    ax1.plot(t,rz)
-    ax1.grid()
-    ax1.set_xlabel("time (s)")
-    ax1.set_ylabel(ylabels["position x"])
-    ax1.set_title("Position")
-
-    # Quaternion
-    ax2 = fig.add_subplot(222)
-    ax2.plot(t,qx)
-    ax2.plot(t,qy)
-    ax2.plot(t,qz)
-    ax2.grid()
-    ax2.set_xlabel("time (s)")
-    ax2.set_title("Quaternion")
-
-    # Position variance
-    ax3 = fig.add_subplot(223)
-    ax3.plot(t,rvarx)
-    ax3.plot(t,rvary)
-    ax3.plot(t,rvarz)
-    ax3.grid()
-    ax3.set_xlabel("time (s)")
-    ax3.set_ylabel(ylabels["r var x"])
-    ax3.set_title("Position Variance")
-
-    # Status
-    ax4 = fig.add_subplot(224)
-    ax4.plot(t,st)
-    ax4.grid()
-    ax4.set_xlabel("time (s)")
-    ax4.set_title("Status")
-
-    # Change xlim
-    if use_my_xlim:
-        ax1.set_xlim(my_xlim)
-        ax2.set_xlim(my_xlim)
-        ax3.set_xlim(my_xlim)
-        ax4.set_xlim(my_xlim)
-
-def plot_debug(fig, df, log_format_filename):
-    # get y-axis labels
-    log_format = json.load(open(log_format_filename))
-    for id in log_format["id"].keys():
-        if log_format["id"][id]["logtype"] == 'debug':
-            labels = log_format["id"][id]["labels"]
-            units = log_format["id"][id]["units"]
-            ylabels = {}
-            for label,unit in zip(labels,units):
-                ylabels[label] = unit
-            break
-
-    t = np.array(df["timestamp"])/1000000.
-    nmr = np.array(df["nav mode request"])
-    fcs = np.array(df["flightctrl state"])
-    rx = np.array(df["position x"])
-    ry = np.array(df["position y"])
-    rz = np.array(df["position z"])
-    gbx = np.array(df["g_b_cmd x"])
-    gby = np.array(df["g_b_cmd y"])
-    q0 = np.array(df["quaternion 0"])
-    qx = np.array(df["quaternion x"])
-    qy = np.array(df["quaternion y"])
-    qz = np.array(df["quaternion z"])
-    tc = np.array(df["thrust cmd"])
-
-    # Nav mode request
-    ax1 = fig.add_subplot(231)
-    ax1.plot(t,nmr)
-    ax1.grid()
-    ax1.set_xlabel("time (s)")
-    ax1.set_title("Nav Mode Request")
-
-    # Flightctrl request
-    ax2 = fig.add_subplot(232)
-    ax2.plot(t,fcs)
-    ax2.grid()
-    ax2.set_xlabel("time (s)")
-    ax2.set_title("FlightCtrl State")
-
-    # Position
-    ax3 = fig.add_subplot(233)
-    ax3.plot(t,rx)
-    ax3.plot(t,ry)
-    ax3.plot(t,rz)
-    ax3.grid()
-    ax3.set_xlabel("time (s)")
-    ax3.set_ylabel(ylabels["position x"])
-    ax3.set_title("Position")
-
-    # Gravity command in b-frame
-    ax4 = fig.add_subplot(234)
-    ax4.plot(t,gbx)
-    ax4.plot(t,gby)
-    ax4.grid()
-    ax4.set_xlabel("time (s)")
-    ax4.set_ylabel(ylabels["g_b_cmd x"])
-    ax4.set_title("Gravity command in b-frame")
-
-    # Quaternion
-    ax5 = fig.add_subplot(235)
-    ax5.plot(t,q0)
-    ax5.plot(t,qx)
-    ax5.plot(t,qy)
-    ax5.plot(t,qz)
-    ax5.grid()
-    ax5.set_xlabel("time (s)")
-    ax5.set_title("Quaternion")
-
-    # Thrust command
-    ax6 = fig.add_subplot(236)
-    ax6.plot(t,tc)
-    ax6.grid()
-    ax6.set_xlabel("time (s)")
-    ax6.set_ylabel(ylabels["thrust cmd"])
-    ax6.set_title("Thrust command")
-
-    # Change xlim
-    if use_my_xlim:
-        ax1.set_xlim(my_xlim)
-        ax2.set_xlim(my_xlim)
-        ax3.set_xlim(my_xlim)
-        ax4.set_xlim(my_xlim)
-        ax5.set_xlim(my_xlim)
-        ax6.set_xlim(my_xlim)
+    ids = list(log_format["id"].keys())
+    id = [id for id in ids if log_format["id"][id]["logtype"] == logtype][0]
+    return id
